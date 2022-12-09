31d49348bf260da5dc79b2a1d0b4b45970328faf
penzslyr <Fendi.prakoso048@gmail.com>
2022-12-09 16:26:28 +0700

    fix fungsi pencarian

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({movie}) => {
   return (
     <div
       className="card bg-gas-money-black w-80 bg-cover bg-no-repeat bg-center shadow-xl my-10 overflow-hidden"
       style={{
-        backgroundImage: "url(/film.png)",
+        backgroundImage: `url(${movie.Poster})`,
         backgroundBlendMode: "luminosity",
       }}
     >
       <div className="h-80"></div>
       <figure>
-        <img src="film.png" alt="film" className="w-52 absolute mb-64" />
+        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt="film" className="w-52 absolute mb-64" />
       </figure>
       <div className="card-body glass rounded-bl-sm rounded-tl-none rounded-tr-full text-wen-minty">
-        <h2 className="card-title text-3xl">Life hack</h2>
-        <p>2022-10-11</p>
-        <p className="text-xl font-bold">7.4</p>
+        <h2 className="card-title text-3xl">{movie.Title}</h2>
+        <p>{movie.Year}</p>
+        <p className="text-xl font-bold">{movie.Type}</p>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchAdded } from '../features/searchSlice';
+
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+  const [searchTerm, setSearchTerm] = useState('');
+  const onClickedSearch =  () => {
+    dispatch(
+      searchAdded(searchTerm)
+    )
+  }
+
   return (
     <>
       <div
-        id="navbar"
-        className="navbar bg-transparent shadow-2xl backdrop-blur-3xl"
+        id='navbar'
+        className='navbar bg-transparent shadow-2xl backdrop-blur-3xl'
       >
-        <div className="flex-1">
-          <a className="btn btn-ghost font-bold normal-case text-xl text-airdrop-white">
+        <div className='flex-1'>
+          <a className='btn btn-ghost font-bold normal-case text-xl text-airdrop-white'>
             FilmsAPI
           </a>
         </div>
-        <div className="flex-none gap-2">
-          <div className="form-control">
+        <div className='flex-none gap-2'>
+          <div className='form-control'>
             <input
-              type="text"
-              placeholder="🔍 Search"
-              className="w-28 md:w-96 input border-t-stake-red border-l-stake-red border-b-alpha-blue border-r-alpha-blue rounded-tl-none rounded-br-none rounded-2xl border-2 bg-gas-money-black text-airdrop-white"
+              type='text'
+              onChange={(e) => setSearchTerm(e.target.value)}
+              value={searchTerm}
+              placeholder='🔍 Search'
+              className='w-28 md:w-96 input border-t-stake-red border-l-stake-red border-b-alpha-blue border-r-alpha-blue rounded-tl-none rounded-br-none rounded-2xl border-2 bg-gas-money-black text-airdrop-white'
             />
           </div>
-          <div className="navbar-end">
-            <a className="btn rounded-tl-none rounded-br-none rounded-2xl w-20 md:w-24 bg-wen-minty text-gas-money-black px-8 hover:bg-alpha-blue">
+          <div className='navbar-end'>
+            <a
+              className='btn rounded-tl-none rounded-br-none rounded-2xl w-20 md:w-24 bg-wen-minty text-gas-money-black px-8 hover:bg-alpha-blue'
+              onClick={() => onClickedSearch(searchTerm)}
+            >
               Search
             </a>
           </div>
