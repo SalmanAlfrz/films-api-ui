6ca267b15368215eca0a3e54a82754a6d12136dc
Salman Alfarizi <salman.alfrz0112@gmail.com>
2022-12-09 22:58:37 +0700

    add about us

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import { useContext, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import Card from './components/Card';
-import Navbar from './components/Navbar';
-import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
-  const searchData = useSelector((state) => state.search.search);
-  const API_URL = 'http://www.omdbapi.com/?apikey=ca3b17a';
-  window.onscroll = () => {
-    const header = document.getElementById('navbar');
-    const fixedNav = header.offsetTop;
-
-    if (window.pageYOffset > fixedNav) {
-      header.classList.add('navbar-fixed');
-    } else {
-      header.classList.remove('navbar-fixed');
-    }
-  };
-
-  const [movieList, setMovieList] = useState();
-
-  useEffect(() => {
-    axios
-      .get(`${API_URL}&s=${searchData}`)
-      .then((res) => {
-        
-        setMovieList(res.data.Search);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [API_URL, searchData]);
-
   return (
-    <div className='mx-auto bg-gradient-to-br from-[#161334]/80 via-[#B286AB] to-[#7493D3] bg-cover'>
-      <Navbar />
-      <div className='flex flex-wrap gap-4 justify-evenly w-5/6 mx-auto'>
-        {movieList?.length > 0 ? (
-          movieList.map((movie) => <Card key={movie.imdbID} movie={movie} />)
-        ) : (
-          <div className='empty'>
-            <h2>No movieList Found</h2>
-          </div>
-        )}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { searchAdded } from '../features/searchSlice';
-
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation()
   const dispatch = useDispatch()
   const [searchTerm, setSearchTerm] = useState('');
-  const onClickedSearch =  () => {
+  const onClickedSearch = () => {
     dispatch(
       searchAdded(searchTerm)
     )
@@ -20,28 +21,30 @@ const Navbar = () => {
       >
         <div className='flex-1'>
           <a className='btn btn-ghost font-bold normal-case text-xl text-airdrop-white'>
-            FilmsAPI
+            <Link to="/">FilmsAPI</Link>
           </a>
         </div>
-        <div className='flex-none gap-2'>
-          <div className='form-control'>
-            <input
-              type='text'
-              onChange={(e) => setSearchTerm(e.target.value)}
-              value={searchTerm}
-              placeholder='🔍 Search'
-              className='w-28 md:w-96 input border-t-stake-red border-l-stake-red border-b-alpha-blue border-r-alpha-blue rounded-tl-none rounded-br-none rounded-2xl border-2 bg-gas-money-black text-airdrop-white'
-            />
+        {location.pathname === '/' && (
+          <div className='flex-none gap-2'>
+            <div className='form-control'>
+              <input
+                type='text'
+                onChange={(e) => setSearchTerm(e.target.value)}
+                value={searchTerm}
+                placeholder='🔍 Search'
+                className='w-28 md:w-96 input border-t-stake-red border-l-stake-red border-b-alpha-blue border-r-alpha-blue rounded-tl-none rounded-br-none rounded-2xl border-2 bg-gas-money-black text-airdrop-white'
+              />
+            </div>
+            <div className='navbar-end'>
+              <a
+                className='btn rounded-tl-none rounded-br-none rounded-2xl w-20 md:w-24 bg-wen-minty text-gas-money-black px-8 hover:bg-alpha-blue'
+                onClick={() => onClickedSearch(searchTerm)}
+              >
+                Search
+              </a>
+            </div>
           </div>
-          <div className='navbar-end'>
-            <a
-              className='btn rounded-tl-none rounded-br-none rounded-2xl w-20 md:w-24 bg-wen-minty text-gas-money-black px-8 hover:bg-alpha-blue'
-              onClick={() => onClickedSearch(searchTerm)}
-            >
-              Search
-            </a>
-          </div>
-        </div>
+        )}
       </div>
     </>
   );
